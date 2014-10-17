0b8d928cee20b5d031274aaf8acee870e741b1e8
kkeenan02 <kkeenan02@qub.ac.uk>
2014-10-17 02:43:11 +0100

    some instructions and contact info

--- ui.R
+++ ui.R
@@ -5,7 +5,7 @@ shinyUI(
   
   pageWithSidebar(
     
-    headerPanel("divMigrate Online"),
+    headerPanel(HTML("<font color = #484587>divMigrate Online</font>")),
     
     sidebarPanel(
       
@@ -32,16 +32,30 @@ shinyUI(
     mainPanel(
         tabsetPanel(
           tabPanel(
-            h5("Usage Instructions"),
-            "Welcome to the divMigrate-Online webb app!"
+            HTML("<h5><font color = #215F9C>Usage Instructions</font></h5>"),
+            HTML("<h3><font color = #215F9C>Welcome to the divMigrate-Online       web app!</font></h3><br><br>"),
+            h4("Getting started:"),
+            HTML("<ol>",
+                 "<li>Read your genepop file (2-digit or 3-digit)</li>",
+                 "<li>Choose which statistic you want to use to calculate",
+                 "relative migration</li>",
+                 "<li>Statistical significance of directional migration ",
+                 "can be calculated by setting the number of bootstraps ",
+                 " to a value > 0</li>",
+                 "<li>The number of links in the network plot can be ",
+                 "manipulated using the 'filter threshold' slider</li>",
+                 "<li>Download your network plot (as a .eps) and your ",
+                 "result matrix (as .txt) for reporting puposes.</li>",
+                 "<br>"),
+            HTML("<font size = 1>Any issues or queries regarding this application can be directed to kkeenan02[at]qub.ac.uk.</font>")
           ),
           tabPanel(
-            h5("Network Plots"),
+            HTML("<h5><font color = #215F9C>Network Plots</font></h5>"),
             downloadButton("dlPlt", "Download Network .eps"),
             plotOutput("plt")           
           ),
           tabPanel(
-            h5("Results Matrix"),
+            HTML("<h5><font color = #215F9C>Results Matrix</font></h5>"),
             h6("To download a file containing your results as a tab",
                "delimited file, please click the button below:"),
             downloadButton("mat", "Download your file!")
